cc68108982758c85b669823f7357931ec58ddd99
Estela Diaz <esteladiaz@fb.com>
2019-06-03 19:31:41 -0700

    Added Header component and style file

---
+++ src/Components/Header/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import SocialMedia from '../SocialMedia';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <SocialMedia />
+        )
+    }
+}
