35911f5e4a45a992764e2a5a6c89b203eeb7ef51
Estela Diaz <esteladiaz@fb.com>
2019-06-06 08:14:01 -0700

    use background color to generate random background color, evenually map to get different colors

--- src/Components/SocialMedia/style.js
+++ src/Components/SocialMedia/style.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components'
+import { getBackgroundColor } from '../../Variables'
+
+const colors = {
+    schemeRed: '#fc7b7b',
+    schemeOrange: '#fcb47b',
+    schemeYellow: '#fcfc7b',
+    schemeGreen: '#7bfc94',
+    schemeBlue: '#7bc3fc',
+    schemeIndigo: '#7b7bfc',
+    schemeViolet: '#bc7bfc'
+}
 
 export const SocialMediaContainer = styled.div`
     display: flex;
+    flex-flow: column wrap;
+    left: 0;
+    height: 100vh;
+    justify-content: space-evenly;
+    margin-left: 1rem;
+    position: absolute;
+    top: 0;
+    a {
+        background: ${getBackgroundColor(colors)};
+        margin-top: 1rem;
+        &:first-child) {
+            margin-top: 0;
+        }
+    }
 `
