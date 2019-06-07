3f9b501357c4e32a9bcfbca88f804b668d7119f7
Estela Diaz <esteladiaz@fb.com>
2019-06-07 13:53:47 -0700

    add test theme

---
+++ src/Themes.js
@@ -0,0 +1,23 @@
+export const theme = {
+    backgroundColorDark: '#232323',
+    backgroundColorLight: '#3c3c3c',
+    codeColor: '#7bfca5',
+    headerColor: '#000',
+    headerBackgroundColor: '#000',
+    headerHoverColor: '#000',
+    linkColor: '#8c7bfc',
+    linkHoverColor: '#da60f5',
+    linkActive: 'purple',
+    textColor: '#fff',
+    schemeRed: '#fc7b7b',
+    schemeOrange: '#fcb47b',
+    schemeYellow: '#fcfc7b',
+    schemeGreen: '#7bfc94',
+    schemeBlue: '#7bc3fc',
+    schemeIndigo: '#7b7bfc',
+    schemeViolet: '#bc7bfc'
+}
+
+export default {
+    theme
+}
