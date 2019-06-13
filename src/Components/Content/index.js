fbbcc5d00e01c76ff15189705bfb80493b1e2bdd
Estela Diaz <esteladiaz@fb.com>
2019-06-13 15:58:57 -0700

    add download resume link

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -4,6 +4,7 @@ import JumpLink from '../../Primitives/JumpLink'
 import Link from '../../Primitives/Link'
 import projects from '../../projects.json'
 import ProjectTile from '../ProjectTile'
+import resume from '../../Assets/resume.pdf'
 
 export default class Content extends Component {
     getProjects = projects => {
@@ -11,16 +12,19 @@ export default class Content extends Component {
             const { name } = project
             return (
                 <ProjectTile key={name} project={project} />
-            )
-        })
-        return projectTile
-    }
-    render() {
-        return (
+                )
+            })
+            return projectTile
+        }
+        render() {
+            console.log(resume, "resume")
+            return (
             <ContentWrapper>
                 <h3>See my <JumpLink text="work" emoji="👩🏽‍💻" /></h3>
-                <h3>Read my <JumpLink text="thoughts" emoji="🤔" /></h3>
-                <h3>Download my <Link text="resume" emoji="📝" /></h3>
+                <h3>Download my <Link src={resume} text="resume" emoji="📝" newTab /></h3>
+                <p>I've mostly worked with Marketing or Growth teams for the past few years.</p>
+                <p>It's fun seeing things I've built out in the wild.</p>
+                <h3><Link text={`Check out some of my work below`} emoji="👇🏽" /></h3>
                 <TileWrapper>
                     {this.getProjects(projects)}
                 </TileWrapper>
