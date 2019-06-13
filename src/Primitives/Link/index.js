990a6bd1fe619c8c1798d80b8f9f0deec1bf8911
Estela Diaz <esteladiaz@fb.com>
2019-06-13 15:59:12 -0700

    add target="_blank" support

--- src/Primitives/Link/index.js
+++ src/Primitives/Link/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 import { LinkWrapper } from './style'
 
 const Link = props => {
-    const { emoji, src, text, toggleSwitch } = props
+    const { emoji, src, newTab, text, toggleSwitch } = props
+    if (newTab) {
+        return (
+            <LinkWrapper href={src} onClick={toggleSwitch} target="_blank">
+                {text}{emoji && ' ' + emoji}
+            </LinkWrapper>
+        )
+    }
     return (
-        <LinkWrapper href={src} onClick={toggleSwitch} >
+        <LinkWrapper href={src} onClick={toggleSwitch}  >
             {text}{emoji && ' ' + emoji}
         </LinkWrapper>
     )
